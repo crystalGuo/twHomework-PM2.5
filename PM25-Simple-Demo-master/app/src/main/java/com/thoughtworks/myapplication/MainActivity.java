8ba948964d5f813ea9436dc2355453039545ea8a
crystal <793832732@qq.com>
2015-12-25 17:43:14 +0800

    version 1.0 to query the quality of city

---
+++ PM25-Simple-Demo-master/app/src/main/java/com/thoughtworks/myapplication/MainActivity.java
@@ -0,0 +1,127 @@
+package com.thoughtworks.myapplication;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.thoughtworks.myapplication.domain.PM25;
+import com.thoughtworks.myapplication.service.AirServiceClient;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText cityEditText;
+    private TextView pm25TextView;
+    private ProgressDialog loadingDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityEditText = (EditText) findViewById(R.id.edit_view_input);
+        pm25TextView = (TextView) findViewById(R.id.text_view_pm25);
+        loadingDialog = new ProgressDialog(this);
+        loadingDialog.setMessage(getString(R.string.loading_message));
+
+
+        findViewById(R.id.button_query_pm25).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onQueryPM25Click();
+            }
+        });
+    }
+
+    public void queryPM25ClickByButton(View v) {
+        switch (v.getId()) {
+            case R.id.button_beijing:
+                cityEditText.setText("beijing");
+                onQueryPM25Click();
+                break;
+            case R.id.button_xian:
+                cityEditText.setText("xian");
+                onQueryPM25Click();
+                break;
+            case R.id.button_shanghai:
+                cityEditText.setText("shanghai");
+                onQueryPM25Click();
+                break;
+            case R.id.button_chengdu:
+                cityEditText.setText("chengdu");
+                onQueryPM25Click();
+                break;
+            case R.id.button_shenzhen:
+                cityEditText.setText("shenzhen");
+                onQueryPM25Click();
+                break;
+            case R.id.button_nanjing:
+                cityEditText.setText("nanjing");
+                onQueryPM25Click();
+                break;
+            case R.id.button_tianjin:
+                cityEditText.setText("tianjin");
+                onQueryPM25Click();
+                break;
+            case R.id.button_wuhan:
+                cityEditText.setText("wuhan");
+                onQueryPM25Click();
+                break;
+        }
+    }
+
+    private void onQueryPM25Click() {
+        final String city = cityEditText.getText().toString();
+        if (!TextUtils.isEmpty(city)) {
+            showLoading();
+            AirServiceClient.getInstance().requestPM25(city, new Callback<List<PM25>>() {
+                @Override
+                public void onResponse(Response<List<PM25>> response, Retrofit retrofit) {
+                    showSuccessScreen(response);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    showErrorScreen();
+                }
+            });
+        }
+    }
+
+    private void showSuccessScreen(Response<List<PM25>> response) {
+        hideLoading();
+        if (response != null) {
+            populate(response.body());
+        }
+    }
+
+    private void showErrorScreen() {
+        hideLoading();
+        pm25TextView.setText(R.string.error_message_query_pm25);
+    }
+
+    private void showLoading() {
+        loadingDialog.show();
+    }
+
+    private void hideLoading() {
+        loadingDialog.dismiss();
+    }
+
+    private void populate(List<PM25> data) {
+        if (data != null && !data.isEmpty()) {
+            PM25 pm25 = data.get(0);
+            pm25TextView.setText("空气质量:"+pm25.getQuality());
+        }
+    }
+}
---
+++ PM25-Simple-Demo-master/app/src/main/java/com/thoughtworks/myapplication/domain/PM25.java
@@ -0,0 +1,49 @@
+package com.thoughtworks.myapplication.domain;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PM25 {
+    @SerializedName("position_name")
+    private String positionName;
+
+    @SerializedName("quality")
+    private String quality;
+
+    @SerializedName("pm2_5_24h")
+    private String pm2_5_24h;
+
+    @SerializedName("area")
+    private String area;
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getPm2_5_24h() {
+        return pm2_5_24h;
+    }
+
+    public void setPm2_5_24h(String pm2_5_24h) {
+        this.pm2_5_24h = pm2_5_24h;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+}
